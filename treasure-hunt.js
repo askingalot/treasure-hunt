486a301b78f7909fca9eb747b10035ba47bd6f83
Andy Collins <askingalot@gmail.com>
2015-11-03 21:28:31 -0600

    refactor and adding simple moveCharacter function

--- treasure-hunt.js
+++ treasure-hunt.js
@@ -1,5 +1,6 @@
 var game = require('./lib/game'),
     actions = require('./lib/actions'),
+    constants = require('./lib/constants'),
     utils = require('./lib/utils');
 
 var gameState = game.initialGameState();
@@ -12,14 +13,33 @@ var quitGameToken = setInterval(function() {
     clearInterval(quitGameToken);
   }
 
-}, 500);
+}, constants.REFRESH_INTERVAL);
 
 function executeFrame(state) {
-  var actionType = utils.getRandomPropertyValue(actions.types),
+  var observation = game.observation(state),
+      actionType = moveCharacter(observation),
       action = { type: actionType },
       newState = game.applyAction(state, action);
 
   game.display(newState);
   return newState;
+}
+
+function moveCharacter(observation) {
+  var obs = observation;
+
+  if (Math.random() > 0.4 && obs.isTreasureNorth() && obs.isNorthClear()) {
+    return actions.types.MOVE_NORTH;
+  }
+  if (Math.random() > 0.4 && obs.isTreasureSouth() && obs.isSouthClear()) {
+    return actions.types.MOVE_SOUTH;
+  }
+  if (Math.random() > 0.4 && obs.isTreasureEast() && obs.isEastClear()) {
+    return actions.types.MOVE_EAST;
+  }
+  if (Math.random() > 0.4 && obs.isTreasureWest() && obs.isWestClear()) {
+    return actions.types.MOVE_WEST;
+  }
+  return actions.types.MOVE_RANDOM;
 }
 
