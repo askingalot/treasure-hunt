662295c8cae6d8540850b4c1ce5004277bb78173
Andy Collins <askingalot@gmail.com>
2015-11-06 10:46:02 -0600

    updating AI

--- treasure-hunt.js
+++ treasure-hunt.js
@@ -28,19 +28,70 @@ function executeFrame(state) {
 
 function moveCharacter(observation) {
   var obs = observation;
+  var randomize = function() { return Math.random() >= 0.5; };
+  var treasureIs = {
+    north: obs.isTreasureNorth() && 
+           !obs.isTreasureEast() && !obs.isTreasureWest(),
+    south: obs.isTreasureSouth() && 
+           !obs.isTreasureEast() && !obs.isTreasureWest(),
+    east: obs.isTreasureEast() && 
+          !obs.isTreasureNorth() && !obs.isTreasureSouth(),
+    west: obs.isTreasureWest() && 
+          !obs.isTreasureNorth() && !obs.isTreasureSouth(),
+    northEast: obs.isTreasureNorth() && obs.isTreasureEast(),
+    northWest: obs.isTreasureNorth() && obs.isTreasureWest,
+    southEast: obs.isTreasureSouth() && obs.isTreasureEast(),
+    southWest: obs.isTreasureSouth() && obs.isTreasureWest()
+  };
 
-  if (Math.random() > 0.4 && obs.isTreasureNorth() && obs.isNorthClear()) {
-    return actions.types.MOVE_NORTH;
+  if (randomize()) {
+    if (treasureIs.north && obs.isNorthClear()) {
+      return actions.types.MOVE_NORTH;
+    }
+    if (treasureIs.south && obs.isSouthClear()) {
+      return actions.types.MOVE_SOUTH;
+    }
+    if (treasureIs.east && obs.isEastClear()) {
+      return actions.types.MOVE_EAST;
+    }
+    if (treasureIs.west && obs.isWestClear()) {
+      return actions.types.MOVE_WEST;
+    }
   }
-  if (Math.random() > 0.4 && obs.isTreasureSouth() && obs.isSouthClear()) {
-    return actions.types.MOVE_SOUTH;
+
+  if (treasureIs.northEast) {
+    if (obs.isEastClear() && randomize()) {
+      return actions.types.MOVE_EAST;
+    }
+    if (obs.isNorthClear() && randomize()) {
+      return actions.types.MOVE_NORTH;
+    }
   }
-  if (Math.random() > 0.4 && obs.isTreasureEast() && obs.isEastClear()) {
-    return actions.types.MOVE_EAST;
+  if (treasureIs.northWest) {
+    if (obs.isWestClear() && randomize()) {
+      return actions.types.MOVE_WEST;
+    }
+    if (obs.isNorthClear() && randomize()) {
+      return actions.types.MOVE_NORTH;
+    }
   }
-  if (Math.random() > 0.4 && obs.isTreasureWest() && obs.isWestClear()) {
-    return actions.types.MOVE_WEST;
+  if (treasureIs.southEast) {
+    if (obs.isEastClear() && randomize()) {
+      return actions.types.MOVE_EAST;
+    }
+    if (obs.isSouthClear() && randomize()) {
+      return actions.types.MOVE_SOUTH;
+    }
   }
+  if (treasureIs.southWest) {
+    if (obs.isWestClear() && randomize()) {
+      return actions.types.MOVE_WEST;
+    }
+    if (obs.isSouthClear() && randomize()) {
+      return actions.types.MOVE_SOUTH;
+    }
+  }
+
   return actions.types.MOVE_RANDOM;
 }
 
